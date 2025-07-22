fb3c4b83dd227f33a577018f47580d3c224186ba
Mr. Fat Guy <2903392+mrfatguy@users.noreply.github.com>
2025-07-22 22:30:04 +0200

    Hopefully final touches for Windows. See #3

--- electron/main.js
+++ electron/main.js
@@ -3,13 +3,16 @@ const path = require('path');
 
 app.whenReady().then(() => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        frame: false,
+        fullscreen: true,
         webPreferences: {
             contextIsolation: true
         }
     });
 
     win.loadFile(path.join(__dirname, '../game/index.html'));
-    win.webContents.openDevTools();
+
+    if (!app.isPackaged) {
+        win.webContents.openDevTools();
+    }
 });
