0b2d0232e45a388a553224f5592f960eb375241f
Mr. Fat Guy <2903392+mrfatguy@users.noreply.github.com>
2025-07-22 15:09:45 +0200

    Change game's directory structure. See #3

--- electron/main.js
+++ electron/main.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 function createWindow () {
     const win = new BrowserWindow({
         fullscreen: true,
         frame: false,
         webPreferences: {
+            sandbox: true,
+            nodeIntegration: false,
             contextIsolation: true
         }
     });
-    win.loadFile('../game/index.html');
-    win.webContents.openDevTools();
+
+    win.loadFile(require('path').join(__dirname, '../src/index.html'));
+
+    if (!app.isPackaged) {
+        win.webContents.openDevTools();
+    }
 }
+
 app.whenReady().then(createWindow);
